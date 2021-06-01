183a284bf3cc2e347a7b5f3a34365f623fcf8d2b
kyc3550 <rmawkxkq0909@naver.com>
2021-06-02 03:51:22 +0900

    이미지 여러개올리기(model수정)

--- community/models.py
+++ community/models.py
@@ -26,11 +26,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_post')
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, db_index=True, unique=True, allow_unicode=True)
-    title_image = models.ImageField(blank=True)
     coment = models.TextField()
     header_coment = models.TextField()
     createData = models.DateTimeField(auto_now=True)
-    updateDate = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True,related_name='posts')
     hits = models.PositiveBigIntegerField(default=0)
     available_post = models.BooleanField('post', default=True)
@@ -40,3 +38,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("community:post_detail", args=[self.id,self.slug])
+
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,null=True)
+    title_image = models.ImageField(upload_to = 'posts/%Y/%m/%d',blank=True)
