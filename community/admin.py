f4df51dadf1c444c229aa9025314d19adc69ae51
kyc3550 <rmawkxkq0909@naver.com>
2021-06-02 16:19:34 +0900

    detail페이지 수정

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [ 'name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','title','slug','coment','createData','category','hits','available_post']
+    prepopulated_fields = {'slug':('title',)}
+    inlines = [PhotoInline]
--- community/urls.py
+++ community/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'community'
 urlpatterns = [
     path('',post_in_category, name='post_all'),
-    path('<slug:category_slug/',post_in_category,name='post_in_category'),
-    path('<int:id>/<post_slug>',post_detail,name='product_detail') #slug: 를 안넣어도 같은동작을 실행
+    path('<slug:category_slug>/',post_in_category,name='post_in_category'),
+    path('<int:id>/<post_slug>',post_detail,name='post_detail') #slug: 를 안넣어도 같은동작을 실행
 ]
